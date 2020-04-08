d7f516e5b5db3199adcb5c837da7bdb76ddee6be
Unixornnn <unixorn@Patricks-MacBook-Pro.local>
2020-04-08 17:19:28 +1000

    - Changed the expanded nodes to include a way of tracing back shortest path - fixed all other methods (BFS, Hill Climbing, Greedy) to use this new strategy - implemented the correct printing format at the end of the program

--- COMPASS1/ThreeDigits.py
+++ COMPASS1/ThreeDigits.py
@@ -22,10 +22,10 @@ for i in restricted_split:
 
 #####################################
 
+
 count = 0
-#expanded_nodes = [(start_value.rstrip("\n"),0)]
-expanded_nodes = [start_value.rstrip("\n") + "0"]
-bfs_expanded = [start_value.strip()]
+expanded_nodes = [start_value.rstrip("\n") + "0" + "0"]
+greedy_expanded = [start_value.rstrip("\n") + "0"]
 results = [start_value.strip("\n")]
 success = False
 
@@ -148,7 +148,7 @@ def lowest_heuristic_index(x, y):
     while i < len(x):
         if str(x[i]) in restricted:
             value.append(1000)
-        elif (x[i],y) in expanded_nodes:
+        elif (str(x) + str(y)) in greedy_expanded:
             value.append(1000)
         else:
             #calculate the manhattan distance for each child node of an expanded node
@@ -171,48 +171,45 @@ def lowest_heuristic_index(x, y):
         i+= 1
     return lowest_index
 
-def check_expanded(x,y):
-    #may be broke, need to check further before further implementation
-    #check whether this node has already been expanded, x is the value, and y is the previously changed value
-    for i in expanded_nodes:
-        if i == (x,y):
-            return True
-        else:
-            return False
-
-def get_heuristic(n,m):
-    first_value = abs(n[0] - m[0])
-    second_value = abs(n[1] - m[1])
-    third_value = abs(n[2] - m[2])
-    final_value = first_value + second_value + third_value
-    return final_value
-
 if test_name == "BFS":
     count = 1
     prev_change = 0
     second = []
     results = [start_value.strip()]
     index = 0
+    posneg = 0
     x = return_children(start_value,0)
     while count <= 1000:
         if count == 1:
             for i in x[1:]:
                 results.append(i)
-                changed_value = abs(int(i) - int(x[0]))
+                changed_value = (int(i) - int(x[0]))
                 if changed_value == 100:
                     prev_change = 1
+                    posneg = 1
+                elif changed_value == -100:
+                    prev_change = 1
+                    posneg = 0
                 elif changed_value == 10:
                     prev_change = 2
+                    posneg = 1
+                elif changed_value == -10:
+                    prev_change = 2
+                    posneg = 0
                 elif changed_value == 1:
                     prev_change = 3
+                    posneg = 1
+                elif changed_value == -1:
+                    prev_change = 3
+                    posneg = 0
                 if i in restricted:
                     continue
-                elif (str(i) + str(prev_change)) in expanded_nodes:
+                elif (str(i) + str(prev_change) + str(posneg)) in expanded_nodes:
                     continue
                 elif int(i) == int(goal):
                     success = True
                     break
-                    break
+                expanded_nodes.append(str(i) + str(prev_change) + str(posneg))
                 second.append(return_children(i,prev_change))
                 count += 1
         else:
@@ -222,22 +219,35 @@ if test_name == "BFS":
                 for a in i[1:]:
                     if success == True:
                         break
-                    changed_value = abs(int(a) - int(i[0]))
+                    changed_value = int(a) - int(i[0])
                     if changed_value == 100:
                         prev_change = 1
+                        posneg = 1
+                    elif changed_value == -100:
+                        prev_change = 1
+                        posneg = 0
                     elif changed_value == 10:
                         prev_change = 2
+                        posneg = 1
+                    elif changed_value == -10:
+                        prev_change = 2
+                        posneg = 0
                     elif changed_value == 1:
                         prev_change = 3
+                        posneg = 1
+                    elif changed_value == -1:
+                        prev_change = 3
+                        posneg = 0
                     if a in restricted:
                         continue
-                    elif (str(a) + str(prev_change)) in expanded_nodes:
+                    elif (str(a) + str(prev_change) + str(posneg)) in expanded_nodes:
                         continue
                     results.append(a)
                     if int(a) == int(goal):
+                        expanded_nodes.append(str(a) + str(prev_change) + str(posneg))
                         success = True
                         break
-                    expanded_nodes.append(str(a) + str(prev_change))
+                    expanded_nodes.append(str(a) + str(prev_change) + str(posneg))
                     second.append(return_children(a,prev_change))
                     count += 1
         x = second
@@ -246,38 +256,7 @@ if test_name == "BFS":
 
 elif test_name == "DFS":
     #code for DFS - left most and continue down until you find a repeated node
-    count = 0
-    prev_change = 0
-    index = 0
-    nodes = []
-    x = return_children(start_value, prev_change)
-    for i in x:
-        nodes.append(i)
-    while count <= 1000:
-        changed_value = abs(int(nodes[index]) - int(nodes[0]))
-        if changed_value == 100:
-            prev_change = 1
-        elif changed_value == 10:
-            prev_change = 2
-        elif changed_value == 1:
-            prev_change = 3
-        node = nodes[index] + str(prev_change)
-        if node in expanded_nodes:
-            index += 1
-            continue
-        elif node[0:3] in restricted:
-            index += 1
-            continue
-        else:
-            expanded_nodes.append(node)
-            count += 1
-            if int(nodes[index]) == int(goal):
-                success = True
-                break
-            x = return_children(node[0:3],node[3])
-            nodes = (nodes[0:index + 1] + x[0:] + nodes[index + 2:])
-            index = 0
-            continue
+    pass
 
 elif test_name == "IDS":
     #code for IDS
@@ -288,83 +267,80 @@ elif test_name == "Greedy":
     #FINISHED
     count = 0
     prev_change = 0
+    posneg = 0
+    node = start_value.strip()
     while count <= 1000:
-        x = return_children(results[count], prev_change)
-        y = lowest_heuristic_index(x,prev_change)
-        changed_value = abs(int(x[y]) - int(x[0]))
+        x = return_children(node, prev_change)
+        original = x[0]
+        x.remove(x[0])
+        y = lowest_heuristic_index(x, prev_change)
+        changed_value = int(x[y]) - int(original)
         if changed_value == 100:
             prev_change = 1
+            posneg = 1
+        elif changed_value == -100:
+            prev_change = 1
+            posneg = 0
         elif changed_value == 10:
             prev_change = 2
+            posneg = 1
+        elif changed_value == -10:
+            prev_change = 2
+            posneg = 0
         elif changed_value == 1:
             prev_change = 3
+            posneg = 1
+        elif changed_value == -1:
+            prev_change = 3
+            posneg = 0
         if x[y] == results[count]:
             success = False
             break
         results.append(x[y])
-        #expanded_nodes.append((x[y],prev_change))
-        expanded_nodes.append(x[y] + str(prev_change))
+        node = x[y]
+        greedy_expanded.append(x[y] + str(prev_change))
         if int(x[y]) == int(goal):
             success = True
             break
         count += 1
 
 elif test_name == "A*":
-    values = []
-    heuristic_values = []
-    node = start_value
-    current = 0
-    count = 0
-    prev_change
-    minimum = 1000
-    depth = 0
-    while count <= 1000:
-        x = return_children(node[0:3],node[4])
-        for i in x[1:]:
-            changed_value = abs(int(x[y]) - int(x[0]))
-            if changed_value == 100:
-                prev_change = 1
-            elif changed_value == 10:
-                prev_change = 2
-            elif changed_value == 1:
-                prev_change = 3
-            values.append((str(node) + str(depth) + str(prev_change))
-        for i in values:
-            heuristic = get_heuristic(i) + i[3]
-            heuristic_values.append(heuristic)
-        for i in heuristic_values:
-            if i < minimum:
-                current = i
-            else:
-                continue
-        index = heuristic_values.index(current)
-        node = values[index]
-        if node in expanded_nodes:
-            
+    pass
     #code for A*
 
 elif test_name == "Hill-climbing":
-    #FINISHED
-    #code for Hill-climbing
-    #implement a value which gives the number of digits that are in the correct position, seeking to increase this number with each move, or at least not decrease
     count = 0
     prev_change = 0
+    posneg = 0
     while count <= 1000:
         x = return_children(results[count], prev_change)
+        original = x.pop(x[0])
         y = lowest_heuristic_index(x,prev_change)
-        changed_value = abs(int(x[y]) - int(x[0]))
+        changed_value = int(x[y]) - int(original)
         if changed_value == 100:
             prev_change = 1
+            posneg = 1
+        elif changed_value == -100:
+            prev_change = 1
+            posneg = 0
         elif changed_value == 10:
             prev_change = 2
+            posneg = 1
+        elif changed_value == -10:
+            prev_change = 2
+            posneg = 0
         elif changed_value == 1:
             prev_change = 3
+            posneg = 1
+        elif changed_value == -1:
+            prev_change = 3
+            posneg = 0
         original_value = x[0]
         new_value = x[y]
         original_heuristic = ((abs(int(original_value[0]) - int(goal[0]))) + (abs(int(original_value[1]) - int(goal[1]))) + (abs(int(original_value[2]) - int(goal[2]))))
         new_heuristic = ((abs(int(new_value[0]) - int(goal[0]))) + (abs(int(new_value[1]) - int(goal[1]))) + (abs(int(new_value[2]) - int(goal[2]))))
         if new_heuristic < original_heuristic:
-            expanded_nodes.append(x[y] + str(prev_change))
+            greedy_expanded.append(x[y] + str(prev_change))
             results.append(x[y])
             count += 1
             if int(x[y]) == int(goal):
@@ -377,8 +353,61 @@ elif test_name == "Hill-climbing":
 else:
     print("error: the test defined is not found within this program.")
 
+#below is how my code handles the values generated in the tests above
+
 if success == True:
     #insert the direct path print here
+    if test_name == "Greedy":
+        for i in results:
+            if results.index(i) == len(results) - 1:
+                print(i)
+            else:
+                print(i,end = ', ')
+    else:
+        current = int(goal)
+        final = []
+        finished = False
+        posneg = 0
+        while True:
+            for i in expanded_nodes:
+                value = int(i[0:3])
+                posneg = int(i[4])
+                changed_value = int(i[3])
+                if value == current:
+                    if len(final) >= 2:
+                        if final[0] == final[1]:
+                            finished = True
+                            break
+                    final.insert(0,value)
+                    if posneg == 1:
+                        #means we added 1 to get to this number, therefore we need to subtract to get new goal
+                        if changed_value == 1:
+                            current = value - 100
+                        elif changed_value == 2:
+                            current = value - 10
+                        elif changed_value == 3:
+                            current = value - 1
+                    elif posneg == 0:
+                        #need to add 1
+                        if changed_value == 1:
+                            current = value + 100
+                        elif changed_value == 2:
+                            current = value + 10
+                        elif changed_value == 3:
+                            current = value + 1
+                else:
+                    pass
+            if finished == True:
+                break
+
+    #correct formatting for printing the above values (final)
+        for i in final[1:]:
+            if final.index(i) == len(final) - 1:
+                print(i)
+            else:
+                print(i, end = ', ')
+
+    #
     for i in results:
         if results.index(i) == len(results) - 1:
             print(i)
@@ -386,4 +415,8 @@ if success == True:
             print(i,end = ', ')
 else:
     print("No solution found.")
-    print(results)
+    for i in results:
+        if results.index(i) == len(results) - 1:
+            print(i)
+        else:
+            print(i,end = ', ')
